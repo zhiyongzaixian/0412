bb3cbd79fc1f22d3713c484cb67b8b86ed1166f8
zhiyong <13852292775@163.com>
2018-09-05 14:39:28 +0800

    'xx'

--- pages/list/list.js
+++ pages/list/list.js
@@ -1,18 +1,24 @@
 // pages/list/list.js
+// 引入数据   只能用相对路劲去引入
+let listData = require('../../datas/list-data.js');
+console.log(listData, typeof listData);
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+		listArr: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+		// 更新数据源
+		this.setData({
+			listArr: listData.list_data
+		})
   },
 
   /**
